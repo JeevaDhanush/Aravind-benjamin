8094266d91ea137407b5f861b4d4e61ecd825608
aravindbenjamin <aravindqwerty08@gmail.com>
2017-06-23 15:40:05 +0530

    Add files via upload

---
+++ Guvi.java
@@ -0,0 +1,18 @@
+package guvi;
+
+public class Guvi {
+	public static void main(String [] args){
+		int a=8;
+		for(int i=0;i<a+1;i++){
+			System.out.print("* ");
+		}
+		System.out.println();
+		for(int i=1;i<a;i++){
+			System.out.println("*\t\t*");
+		}
+		for(int i=0;i<a+1;i++){
+			System.out.print("* ");
+		}
+		
+	}
+}
